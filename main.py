188a0a715ca98251b67ef3fbd790aeffaf13bc51
unknown <Aneeq.KHAN@student.howest.be>
2026-03-17 05:56:21 +0100

    feat: add species model, repository and router

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
-
+from database import start_db
+from routers import species 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    start_db()   
+
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    start_db()
+
+app.include_router(species.router)
